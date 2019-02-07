2b0706db62fd1b2dea7b06021bcca33e9e64377a
Simon <01secn@gmail.com>
2019-02-07 11:41:15 +0000

    Refactored price object to not store formatted price variable but instead have get formatted price method that formats and returns the formatted price when requested

--- lib/item.rb
+++ lib/item.rb
@@ -1,15 +1,13 @@
 class Item
     attr_reader :price
-    attr_reader :formatted_price
     DEFAULT_PRICE = 20.00
 
     def initialize
         @price = DEFAULT_PRICE
-        set_formatted_price
     end
 
-    def set_formatted_price
+    def get_formatted_price
         price_2_dec_places = '%.2f' % @price
-        @formatted_price = '£' << price_2_dec_places.to_s
+        '£' << price_2_dec_places.to_s
     end
 end
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -4,6 +4,6 @@ describe Item do
   it 'can display its price in the format "£xx.xx"' do
     item = Item.new
 
-    expect(item.formatted_price).to eq('£20.00')
+    expect(item.get_formatted_price).to eq('£20.00')
   end
 end
