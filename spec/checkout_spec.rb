5461db31e31e7ae832664d28a783c7ed45c33089
Simon <01secn@gmail.com>
2019-02-07 10:43:27 +0000

    Refactored checkout tests - moved checkout instantation into before each

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -1,17 +1,18 @@
 require './lib/checkout.rb'
 
 describe Checkout do
+    before(:each) do
+        @checkout = Checkout.new
+    end
+
     it 'can scan items' do
-      checkout = Checkout.new
       item = Item.new
   
       expect { checkout.scan(item) }.not_to raise_error
     end
 
     it 'can display price for all scanned items' do
-        checkout = Checkout.new
         no_of_items = 5
-        
         no_of_items.times do
             checkout.scan(Item.new)
         end
